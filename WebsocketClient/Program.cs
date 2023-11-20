81a6a0a03fe34f75ad532cf24560e8aeed41be0d
Eetu Asikainen <eetu.asikainen@visma.com>
2023-11-20 13:28:14 +0200

    Config base

--- WebsocketClient/Program.cs
+++ WebsocketClient/Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Net.WebSockets;
-using System.Text;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using WebsocketClient.Logging;
 using WebsocketClient.Wrapper;
@@ -10,6 +7,7 @@ class Program
 {
     private static async Task Main(string[] args)
     {
+        var config = new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true).Build();
         const string logFilePath = "../../../../test_log.log";
         await using var logFileWriter = new StreamWriter(logFilePath, append: false);
         using var loggerFactory = LoggerFactory.Create(builder =>
@@ -17,8 +15,7 @@ class Program
             builder.AddConsole();
             builder.AddProvider(new FileLoggerProvider(logFileWriter));
         });
-        var uriString = "ws://localhost:8765";
         var client = new Client(loggerFactory);
-        await client.Run(uriString, "myBotToken", "myBotName");
+        await client.Run(config["Client:WebSocketUrl"], config["Client:Token"], config["Client:BotName"]);
     }
 }
